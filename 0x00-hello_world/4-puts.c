8ea1bf2d1b774eaff3305a1e16c3783f461f90dc
Ololade Victor <oluwabori2014@gmail.com>
2023-03-16 07:29:46 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * returns string
+ */
+
+int main(void)
+{
+	char word[] = "Programming is like building a multilingual puzzle \n";
+
+	puts(word);
+
+	return (0);
+}
