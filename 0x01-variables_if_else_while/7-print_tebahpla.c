1aff68510ac1808c2e90ec003251f4d868533bd5
Ololade Victor <oluwabori2014@gmail.com>
2023-03-17 17:55:01 -0700

    Alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; --i)
+		printf("%c", i);
+	printf("\n");
+	return (0);
+}
