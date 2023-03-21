8dc458e6a4ea281cc69a5a6aabd91be301245301
Ololade Victor <oluwabori2014@gmail.com>
2023-03-21 09:02:31 -0700

    Prints 10 times the alphabet in lowercase followed by a new line using a new header file

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry point
+ * Return: Always 0 (Success)
+ */
+void print_alphabet_x10(void)
+{
+	int j;
+	char i;
+
+	for (j = 0; j <= 9; ++j)
+	{
+		for (i = 'a'; i <= 'z'; ++i)
+		{
+			_putchar (i);
+		}
+		_putchar ('\n');
+	}
+}
