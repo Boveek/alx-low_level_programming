e733e3677a482dfd2c439a01d20ab063538cf650
Ololade Victor <oluwabori2014@gmail.com>
2023-03-22 12:45:16 -0700

    A function that prints the multipliction table from 0 up to a given number using a designed headerfile

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -26,8 +26,8 @@ void print_times_table(int n)
 			else if ((k >= 10) && (k < 100))
 			{
 				_putchar (' ');
-				_putchar ((k % 10) + '0');
 				_putchar ((k / 10) + '0');
+				_putchar ((k % 10) + '0');
 			}
 			else if (j == 0)
 				_putchar (k + '0');
