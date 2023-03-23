dbc975518a73e95959d0fc35c4008a8d78b94609
Ololade Victor <oluwabori2014@gmail.com>
2023-03-23 06:25:16 -0700

    A function that multiplies two integers using a designed headerfile

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - writes the sum of an integer
+ * @a: The character to add
+ * @b: The character to add
+ * Return: On success 1.
+ */
+int mul(int a, int b)
+{
+	int c = a * b;
+
+	return (c);
+}
