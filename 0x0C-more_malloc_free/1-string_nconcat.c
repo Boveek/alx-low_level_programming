982a3fccdd4a10402945e16c79d0ed368cdf2e8f
Ololade Victor <oluwabori2014@gmail.com>
2023-04-13 09:55:27 -0700

    A function that concatenate a string to another to a number of n

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,54 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * string_nconcat - A function that concatenates a string to
+ * another string to the number of n
+ * @s1: Input string 1
+ * @s2: input string 2
+ * @n: Number of string 2 to be concatenated
+ * Return: A pointer to the concatenated string on Success
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, k, s2count = 0, s1count = 0;
+	char *ptr;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (!(s1 == NULL))
+			s1count++;
+	}
+	for (i = 0; s2[i] != '\0'; i++)
+	{
+		if (!(s2 == NULL))
+			s2count++;
+	}
+	if (n < s2count)
+	{
+		ptr = malloc(sizeof(char) * (s1count + n + 1));
+		if (ptr == NULL)
+			return (NULL);
+		for (i = 0; i < s1count; i++)
+			ptr[i] = s1[i];
+		for (k = 0; k < n; k++)
+		{
+			ptr[i++] = s2[k];
+		}
+		ptr[i] = '\0';
+	}
+	if (n >= s2count)
+	{
+		ptr = malloc(sizeof(char) * (s1count + s2count + 1));
+		if (ptr == NULL)
+			return (NULL);
+		for (i = 0; i < s1count; i++)
+			ptr[i] = s1[i];
+		for (k = 0; k < s2count; k++)
+		{
+			ptr[i++] = s2[k];
+		}
+		ptr[i] = '\0';
+	}
+	return (ptr);
+}
