712c43ccd39eadaa8327af4f48f57b3f0d8457d4
Ololade Victor <oluwabori2014@gmail.com>
2023-04-19 08:05:16 -0700

    A function that searches for an integer and returns the index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+/**
+ * int_index - function that prints the index of a compared integer
+ * @array: pointer to an array
+ * @size: size of the array
+ * @cmp: function that compares integers
+ * Return: returns index of the integer on success
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0 || array == NULL)
+		return (-1);
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) == 0)
+			return (-1);
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
