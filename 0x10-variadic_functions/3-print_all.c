4f18205a06b65e92cae7234c9642e364e5079389
Ololade Victor <oluwabori2014@gmail.com>
2023-04-20 16:30:37 -0700

    A function that prints anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,52 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_all - A function that prints anything
+ * @format: types of arguments that are passed to the function
+ */
+void print_all(const char * const format, ...)
+{
+	int i;
+	char *ptr;
+	char *separator = "";
+
+	va_list a;
+	va_start(a, format);
+
+	if (format)
+	{
+		i = 0;
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", separator, va_arg(a, int));
+					break;
+				case 'i':
+					printf("%s%d", separator, va_arg(a, int));
+					break;
+				case 'f':
+					printf("%s%f", separator, va_arg(a, double));
+					break;
+				case 's':
+					ptr = va_arg(a, char *);
+					if (!ptr)
+						ptr = "(nil)";
+					printf("%s%s", separator, ptr);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			separator = ", ";
+			i++;
+		}
+	}
+
+	printf("\n");
+	va_end(a);
+}
+
