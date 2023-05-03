fff53c787ad77e325fde13a3da6a7403c78a58ba
Boveek <oluwabori2014@gmail.com>
2023-05-03 10:08:57 -0700

    A function that prints a linked list

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 #include <stdio.h>
 /**
- *
- *
- *
- *
+ * print_listint_safe - function that prints a linked
+ * list that has an infinte loop
+ * @head: pointer to the address of the first element in the list
+ * Return: returns the number of nodes printed
  */
 size_t print_listint_safe(const listint_t *head)
 {
@@ -13,19 +13,19 @@ size_t print_listint_safe(const listint_t *head)
 
 	while (now && noww && noww->next)
 	{
-		printf("[%p] %d\n", (void*)now, now->n);
+		printf("[%p] %d\n", (void *)now, now->n);
 		count++;
 		now =  now->next;
 		noww = noww->next->next;
 		if (now == noww)
 		{
-			printf("-> [%p] %d\n", (void*)now, now->n);
-			return(count);
+			printf("-> [%p] %d\n", (void *)now, now->n);
+			return (count);
 		}
 	}
 	while (now)
 	{
-		printf("[%p] %d\n", (void*)now, now->n);
+		printf("[%p] %d\n", (void *)now, now->n);
 		count++;
 		now = now->next;
 	}
