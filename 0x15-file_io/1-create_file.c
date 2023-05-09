2fa94d679ce0229078c4830c8c6f3b24f11e0b56
Boveek <oluwabori2014@gmail.com>
2023-05-08 18:18:09 -0700

    A function that creates a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 	num = strlen(text_content);
-	count = write(fd, text_content, num + 1);
+	count = write(fd, text_content, num);
 	if (count == -1)
 		return (-1);
 	close(fd);
