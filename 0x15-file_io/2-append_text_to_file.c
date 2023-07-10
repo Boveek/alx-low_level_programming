0cf2aa16cbf9c3cd083beba8eca78f138aa2b5ee
Ololade Victor <oluwabori2014@gmail.com>
2023-07-10 14:45:30 -0700

    C-file io

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * append_text_to_file - function that appends a string in a file
+ * @filename: files passed in
+ * @text_content: content of the file to be appended
+ * Return: returns 1 on success and -1 for error
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int count, num;
+	int fd;
+
+	if (filename == NULL)
+		return (-1);
+	fd = open(filename, O_WRONLY | O_APPEND);
+	if (fd == -1)
+		return (-1);
+	if (text_content)
+	{
+	count = strlen(text_content);
+	num = write(fd, text_content, count);
+	if (num == -1)
+		return (-1);
+	}
+	close(fd);
+	return (1);
+}
