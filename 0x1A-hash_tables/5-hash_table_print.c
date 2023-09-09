01be2362b1ed8a872a68213869d9a3cfff331530
Ololade Victor <oluwabori2014@gmail.com>
2023-09-08 19:53:30 -0700

    hash tables

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print -  function that prints a hash table
+ * @ht: the hash table to be printed
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node;
+	bool check = false;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		if (ht->array != NULL)
+		{
+			node = ht->array[i];
+			while (node != NULL)
+			{
+				if (check == true)
+					printf(", ");
+				printf("'%s' : '%s'", node->key, node->value);
+				node = node->next;
+				if (node != NULL)
+					printf(", ");
+				check = true;
+			}
+		}
+	}
+	printf("}\n");
+}
