474a2a5345f25de8acc0e5e447745734faa35799
Ololade Victor <oluwabori2014@gmail.com>
2023-09-08 20:17:49 -0700

    hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,7 +13,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (new_table == NULL)
 		return (NULL);
 	new_table->size = size;
-	new_table->array = calloc(size, sizeof(hash_node_t));
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (new_table->array == NULL)
 		return (NULL);
 	for (i = 0; i < new_table->size; i++)
